fdec5d9e513edb609a43498bc69e44379261aefb
DanYT2 <danwwaititu@gmail.com>
2023-08-18 22:06:09 +0300

    feature(types): fixed types for current users

---
+++ app/types/index.ts
@@ -0,0 +1,11 @@
+import {User} from '@prisma/client'
+import src from "react-hot-toast/src";
+
+export type SafeUser = Omit<
+  User,
+  "createdAt" | "updatedAt" | "emailVerified"
+> & {
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
+}
